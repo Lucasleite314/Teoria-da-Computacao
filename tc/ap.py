9b9db68deb275021244da585936542a8187d41dd
Lucas Leite <lucas.andrade@intcom.com.br>
2021-05-26 00:28:09 -0300

    Update ap.py

--- tc/ap.py
+++ tc/ap.py
@@ -7,3 +7,26 @@ def delta(automato, estado, simbolo, pilha):
     except:
         return (None,None)
 
+#52 Transformação GLC para AP (Lucas Leite) 
+
+	class Transformar:
+    simbolo = ''
+    estado = ''
+    novo_estado = ''
+    desempilharSimbolo = ''
+    empilharSimbolo = []
+    
+    def __init__(self, simbolo, estado, novo_estado, desempilharSimbolo, empilharSimbolo):
+        self.simbolo = simbolo
+        self.estado = estado
+        self.novo_estado = novo_estado
+        self.desempilharSimbolo = desempilharSimbolo
+        self.empilharSimbolo = empilharSimbolo
+    
+    def addEmpilharSimbolo(self, sym):
+        self.empilharSimbolo.add(sym)
+    
+    def __str__(self):
+        empilharString = ''.join(self.empilharSimbolo)
+        string = self.simbolo + ", " + self.desempilharSimbolo + ", " + empilharString 
+        return string
